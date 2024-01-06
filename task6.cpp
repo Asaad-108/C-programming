e7e4c1224e0608fe309a53a05e360698a128bb1a
Asaad-108 <143253998+Asaad-108@users.noreply.github.com>
2024-01-06 15:46:55 +0500

    Add files via upload

---
+++ task6.cpp
@@ -0,0 +1,30 @@
+// Use of charactor variables 
+// Input and Output instructions
+#include<iostream>
+using namespace std;
+int main ()
+{
+    char a,b,c,d,e,f;
+    cout<<"enter name:"<<endl;
+    cin>>a;
+    cout<<"enter city:"<<endl;
+    cin>>b;
+    cout<<"enter age:"<<endl;
+    cin>>c;
+    cout<<"enter university:"<<endl;
+    cin>>d;
+    cout<<"enter profession:"<<endl;
+    cin>>e;
+    cout<<"enter pet name:"<<endl;
+    cin>>f;
+
+    cout<<"there once was a person named "<<a;
+    cout<<" who lived in "<<b<<endl;
+    cout<<" at the age of "<<c;
+    cout<<a<<" went to university at college "<<d;
+    cout<<a<< " graduated and went to work as a     "<<e;
+    cout<< "    then,"<<a;
+    cout<<"   adopted a pet "<<f;
+
+    return 0;
+}
